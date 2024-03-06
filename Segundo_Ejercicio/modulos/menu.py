3c7de2447a9c9fa758cf5913a1a633676240c63a
jorgejaimesg <158189835+Jorgejaimesg@users.noreply.github.com.>
2024-03-06 11:42:33 -0500

    primero y parte del segundo

---
+++ Segundo_Ejercicio/modulos/menu.py
@@ -0,0 +1,55 @@
+import modulos.coreFiles1 as cf
+def menu():
+    estudiantes={}
+    cf.checkFile('estudiantes.json',estudiantes)
+    global data_estudiantes
+    data_estudiantes=cf.readDataFile('estudiantes.json')
+
+    title="""
+    #############################
+    ####### ESTUDIANTES #########
+    #############################
+    """
+
+    isRunning=True
+    while isRunning:
+        print (title)
+
+        print('1. Agregar \n2. Buscar\n3. Salir')
+        op=input('->')
+        if op=='1':
+            nombre=cf.checkinput('str','Ingrese el nombre')
+            apellido=cf.checkinput('str','Ingrese el o los apellidos')
+            dirección=cf.checkinput('str','Ingrese la direccion')
+            ciudad=cf.checkinput('str','Ingrese la ciudad')
+            longitud=cf.checkinput('float','Ingrese la longitud')
+            latitud=cf.checkinput('float','Ingrese la latitud')
+            email=cf.checkinput('str','Ingrese el email')
+            edad=cf.checkinput('int','Ingrese la edad')
+            ocupación= cf.checkinput('str','Ingrese la ocupacion')
+
+            estudiante={
+                'nombre':nombre,
+                'apellido':apellido,
+                'dirección':dirección,
+                'ubicacion':{
+                    'ciudad':ciudad,
+                    'longitud':longitud,
+                    'latitud':latitud},
+                'email':email,
+                'edad':edad,
+                'ocupación':ocupación
+            }
+            data_estudiantes.update({nombre:estudiante})
+            cf.addData('estudiantes.json',data_estudiantes)
+        elif op=='2':
+            buscar=input('ingrese el nombre del estudiante')
+            if buscar in data_estudiantes:
+                print(data_estudiantes[nombre])
+            else:
+                print('El estudiante no existe')
+                
+        elif op=='3':
+            isRunning=False
+        else:
+            menu()
